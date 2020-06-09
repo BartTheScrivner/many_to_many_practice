d1f7ad2fa4fbccb2cc8666256f04cea92a6a30ed
BartTheScrivner <dframe22@gmail.com>
2020-06-09 13:03:25 -0400

    built models. Tourist passing through .find_by_name

--- app/models/tourist.rb
+++ app/models/tourist.rb
@@ -1,1 +1,19 @@
 # put your Tourist model here
+class Tourist
+  attr_reader :name
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|tourist| tourist.name == name}
+  end
+
+
+end
--- tools/console.rb
+++ tools/console.rb
@@ -2,4 +2,48 @@ require_relative '../config/environment.rb'
 
 # create your variables and/or write any tests here
 
-binding.pry
+
+#Tourists
+bob = Tourist.new("Bob")
+dave = Tourist.new("Dave")
+issa = Tourist.new("Issa")
+
+#Landmarks
+ladyliberty = Landmark.new("Statue of Liberty", "New York City")
+sfbridge = Landmark.new("Golden Gate Bridge", "San Francisco")
+
+#Trips
+bob_at_ladyliberty = Trip.new(bob, ladyliberty)
+dave_at_sfbridge = Trip.new(dave, sfbridge)
+
+puts "Tourist Tests"
+puts "has a name"
+puts bob.name == "Bob"
+puts "have a .all method"
+puts bob 
+puts ".all to return all instances"
+pp Tourist.all 
+puts "Tourist.find_by_name, given a string of a name, returns the **first tourist** whose  name matches"
+puts Tourist.find_by_name("Dave") == dave
+pp Tourist.find_by_name("Dave")
+
+puts "Landmark Test"
+puts "has a name"
+puts ladyliberty.name == "Statue of Liberty"
+
+puts "has a city "
+puts ladyliberty.city == "New York City"
+
+puts ".all returns all"
+pp Landmark.all
+# binding.pry
+
+#Trip
+
+  puts ".all returns an array of all trips"
+  pp Trip.all
+ puts "#tourist returns the tourist who has taken that trip"
+  puts bob_at_ladyliberty.tourist == bob
+ puts "#landmark returns the landmark visited on the trip"
+  puts dave_at_sfbridge.landmark == sfbridge
+
