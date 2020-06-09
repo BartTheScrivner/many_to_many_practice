e6cbb6591cee6f0b0896b85100eeb94c9c2b6787
Issa sangare <issabmsangare@hotmail.com>
2020-06-09 16:17:01 -0400

    Solved all methods except Landmark's ones

--- app/models/tourist.rb
+++ app/models/tourist.rb
@@ -1,4 +1,5 @@
 # put your Tourist model here
+
 class Tourist
   attr_reader :name
   @@all = []
@@ -15,5 +16,36 @@ class Tourist
     self.all.find {|tourist| tourist.name == name}
   end
 
+  def trips
+    Trip.all.select do |trip|
+      trip.tourist == self
+    end
+  end
 
+  def landmarks
+    # trips = Trip.all.select do |trip|
+    #   trip.tourist == self
+    # #  self.trips
+    # end
+    self.trips.map do |trip|
+      trip.landmark 
+    end
+  end
+
+  def visit_landmark(landmark)
+    Trip.new(self, landmark)
+  end
+
+  def never_visited
+    Landmark.all.select do |landmark|
+      !self.landmarks.include?(landmark)
+      binding.pry
+    end
+  #   trips_not_visited  = Trip.all.select do |trip|
+  #     trip.tourist != self
+  #   end
+  #   trips_not_visited.map do |trip|
+  #     trip.landmark if !self.landmarks.include?(trip.landmark)
+  #   end
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -11,10 +11,12 @@ issa = Tourist.new("Issa")
 #Landmarks
 ladyliberty = Landmark.new("Statue of Liberty", "New York City")
 sfbridge = Landmark.new("Golden Gate Bridge", "San Francisco")
+lincoln_mem = Landmark.new("Lincoln Memorial", "DC")
 
 #Trips
 bob_at_ladyliberty = Trip.new(bob, ladyliberty)
 dave_at_sfbridge = Trip.new(dave, sfbridge)
+dave_at_lincoln_memo = Trip.new(dave, lincoln_mem)
 
 puts "Tourist Tests"
 puts "has a name"
@@ -26,6 +28,13 @@ pp Tourist.all
 puts "Tourist.find_by_name, given a string of a name, returns the **first tourist** whose  name matches"
 puts Tourist.find_by_name("Dave") == dave
 pp Tourist.find_by_name("Dave")
+puts "returns an **array** of all the trips taken by the given `Tourist`"
+pp dave.trips.include?(dave_at_sfbridge)
+puts "returns an **array** of all the landmarks for the given `Tourist`"
+pp bob.landmarks.include?(ladyliberty)
+puts "should create a new trip for that tourist to the given landmark"
+pp issa.visit_landmark(sfbridge)
+pp dave.never_visited
 
 puts "Landmark Test"
 puts "has a name"
